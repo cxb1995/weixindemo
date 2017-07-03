33e6077dc2b7d782477c6bf6c0318b79bd10a536
DESKTOP-FPGN5SB\cxb <18336070693@163.com>
2017-07-03 11:12:24 +0800

    微信小城序个人主页

---
+++ 我的第一个小程序/pages/person/person.js
@@ -0,0 +1,39 @@
+Page({
+  data:{
+        nename:"大北",
+        address:"北京",
+        faxing:196,
+        jiage:380,
+        yuyue:1,
+        addre:"乔司国际",
+        dizhi:"将达公路36号",
+        bgimg:"http://img.hairbobo.com/uploadimg/17/05/01/p170501232430339.jpg",
+        logoimg:"http://img.hairbobo.com/uploadimg/17/06/14/170614114806930.jpg",
+        showimg:["../../image/timg.jpg","../../image/head.jpg","../../image/top.jpg","../../image/timg.jpg","../../image/head.jpg","../../image/top.jpg","../../image/timg.jpg","../../image/head.jpg"],
+        dingping:false
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    // console.log(options)
+    // console.log(options.url)
+    // this.setData({
+    //   url: options.url,
+    //   height: options.height,
+    //   width: options.width,
+    //   loadingHidden: true
+
+    // })
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
